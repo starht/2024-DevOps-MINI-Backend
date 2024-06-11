f6156b856fe7a815b0e3a664acc6038fc3ff5586
starht <star2thb@gmail.com>
2024-06-11 10:24:57 +0900

    Feat: User

--- src/main/java/com/example/minibackend/entity/User.java
+++ src/main/java/com/example/minibackend/entity/User.java
@@ -1,4 +1,17 @@
 package com.example.minibackend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
 }
