6c429fc7050f0029da9487eb4dea3a62b64e7270
starht <star2thb@gmail.com>
2024-06-11 09:32:33 +0900

    first commit

---
+++ MiniBackend/src/main/java/mini/MiniBackendApplication.java
@@ -0,0 +1,13 @@
+package mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MiniBackendApplication.class, args);
+  }
+
+}
---
+++ MiniBackend/src/test/java/mini/MiniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
