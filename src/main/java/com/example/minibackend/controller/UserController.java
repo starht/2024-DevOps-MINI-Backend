0c3ae4304ad3e113f0a75d44e0bd427da15ea94f
starht <star2thb@gmail.com>
2024-06-13 09:28:03 +0900

    Feat: user 수정

--- src/main/java/com/example/minibackend/controller/UserController.java
+++ src/main/java/com/example/minibackend/controller/UserController.java
@@ -7,38 +7,38 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/userinfo")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+  private final UserService userService;
 
-    @GetMapping
-    public List<User> getUsers() {
-        return userService.getAllUsers();
-    }
+  @GetMapping
+  public List<User> getUsers() {
+    return userService.getAllUsers();
+  }
 
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable("id") int id) {
-        return userService.getUserById(id);
-    }
+  @GetMapping("/{userId}")
+  public Optional<User> getUser(@PathVariable("userId") String userId) {
+    return userService.findByUserId(userId);
+  }
 
-    @PostMapping("/add")
-    public User addUser(@RequestBody User user) {
-        UserDTO userDTO = new UserDTO(0,user.getUserId(), user.getPassword(), user.getName(), user.getBmi());
-        return
-                userService.addUser(userDTO);
-    }
+  @PostMapping("/add")
+  public User addUser(@RequestBody User user) {
+    UserDTO userDTO = new UserDTO(0,user.getUserId(), user.getPassword(), user.getName(), user.getBmi());
+    return userService.addUser(userDTO);
+  }
 
-    @PutMapping("/update")
-    public User updateUser(@RequestBody UserDTO userDTO) {
-        User updatedUser = userService.updateUser(userDTO);
-        return updatedUser;
-    }
+  @PutMapping("/update")
+  public User updateUser(@RequestBody UserDTO userDTO) {
+    User updatedUser = userService.updateUser(userDTO);
+    return updatedUser;
+  }
 
-    @DeleteMapping("/delete")
-    public void deleteUser(@RequestBody UserDTO userDTO) {
-        userService.deleteUserById(userDTO.getId());
-    }
+  @DeleteMapping("/delete")
+  public void deleteUser(@RequestParam("id") int id) {
+    userService.deleteUserById(id);
+  }
 }
