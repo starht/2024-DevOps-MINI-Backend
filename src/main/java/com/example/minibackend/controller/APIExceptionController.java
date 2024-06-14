ce9e92fb723bbd821e13beac3d34f236af999f0e
ok2385 <hhj4669@gmail.com>
2024-06-14 14:11:52 +0900

    Runtime Exception외 다른예외 처리

--- src/main/java/com/example/minibackend/controller/APIExceptionController.java
+++ src/main/java/com/example/minibackend/controller/APIExceptionController.java
@@ -2,6 +2,7 @@ package com.example.minibackend.controller;
 
 
 import com.example.minibackend.error.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class APIExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -22,6 +24,14 @@ public class APIExceptionController {
     @ExceptionHandler(RuntimeException.class)
     public ErrorResult handleInternalServerError(RuntimeException ex) {
         String code = "런타임 에러";
+        log.info("에러 메세지: {}", ex.getMessage());
+        return new ErrorResult(code, ex.getMessage());
+    }
+
+    @ResponseStatus
+    @ExceptionHandler(Exception.class)
+    public ErrorResult handleException(Exception ex) {
+        String code = "unknown error";
         return new ErrorResult(code, ex.getLocalizedMessage());
     }
 }
